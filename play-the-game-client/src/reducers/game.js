405292b852fb312a043bf09b76da07752cc3ce43
sgibson47 <sgibson47@gmail.com>
2018-12-12 15:35:24 -0600

    update game reducer to update top card of a pile

--- play-the-game-client/src/reducers/game.js
+++ play-the-game-client/src/reducers/game.js
@@ -45,6 +45,17 @@ export default (state = initialState, action) => {
   switch(action.type){
     case 'GET_GAME_SUCCESS':
       return action.currentGame
+
+    case 'UPDATE_TOP_CARD':
+      let newPiles = state.piles.map(pile => {
+        if (pile.id === action.data.pile_id) {
+          pile.topMostCard = action.data.card;
+          return pile;
+        } else {
+          return pile;
+        }
+      });
+      return Object.assign({}, state, {piles: newPiles});
     
     default:
       return state;
