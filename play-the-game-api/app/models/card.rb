e65d815c75d44d65b51701fb940430625a5755b6
sgibson47 <sgibson47@gmail.com>
2018-12-14 14:55:54 -0600

    fix typo in mostRecentlyPlayed; add brackets containing a zero to the end of code inside topMostCard to serialize the sole card object in the array instead of the array; comment out .then logging out the response from updating agame; uncomment out the .then calls updating the redux store's currentGame and clearing its moves

--- play-the-game-api/app/models/card.rb
+++ play-the-game-api/app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :whereIsCard, polymorphic: true
 
-  scope :mostRecetlyUpdated, -> { order(updated_at: :desc) }
+  scope :mostRecentlyUpdated, -> { order(updated_at: :desc) }
   scope :one, -> { limit(1) }
 end
--- play-the-game-api/app/serializers/pile_serializer.rb
+++ play-the-game-api/app/serializers/pile_serializer.rb
@@ -4,6 +4,24 @@ class PileSerializer < ActiveModel::Serializer
   # has_many :cards, polymorphic: true
 
   def topMostCard
-    object.cards.mostRecetlyUpdated.one
+    object.cards.mostRecentlyUpdated.one[0]
+    # nope, this didn't work
+    # b/c scope methods work on the class Card, not on a collection of cards?
+
+    # well it works in the console
+    # it returns the collection of cards in order of most recently updated
+
+    # lets go to rested & see what this is outputting
+    # mmk, it's returning what we'd expect
+    # BUT the topMostCard object is now the sole element in an array
+
+    # we have what we want, let's just rework
+    # the front end to expect an array instead of an object
+    # OR 
+    # could we access the first element of the array here
+    # and just serialize an object?
+
+    # groovy with [0] we send out jsut the object 
+
   end
 end
