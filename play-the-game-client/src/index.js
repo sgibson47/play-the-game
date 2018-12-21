c8e4394a587892656d38b08525ead105fa30c1a1
sgibson47 <sgibson47@gmail.com>
2018-12-21 13:59:44 -0500

    import BrowserRouter from react-router-dom; replace rendering App component with react router and a route that renders the App component when a user navigates to app's base url ('/')

--- play-the-game-client/src/index.js
+++ play-the-game-client/src/index.js
@@ -5,10 +5,13 @@ import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import store from './store'
 import {Provider} from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path='/' component={App}/>
+    </Router>
   </Provider>, 
   document.getElementById('root'));
 
