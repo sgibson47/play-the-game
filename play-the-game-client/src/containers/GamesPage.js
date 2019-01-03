d08470afd8d1669753b74f108fba21aa157c8dcd
sgibson47 <sgibson47@gmail.com>
2019-01-03 15:15:31 -0600

    create new GamesPage contianer component to eventually hold games index

---
+++ play-the-game-client/src/containers/GamesPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import GamesList from '../components/GamesList';
+
+const GamesPage = ({ games }) => (
+  <div>
+    <GamesList games={games} />
+  </div>
+)
+
+export default GamesPage
