1fc73819f5d0ed5361cee19c0037c7b5b07758e4
sgibson47 <sgibson47@gmail.com>
2019-01-03 16:45:57 -0600

    refactor GamesList to produce an unordered list that acturately link's to particular game's url and display the playerName for that game

--- play-the-game-client/src/components/GamesList.js
+++ play-the-game-client/src/components/GamesList.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const GamesList = ({ games }) => {
-  const renderGames = Object.keys(games).map(game =>
-    <Link key={game.id} to={`/games/${game.id}`}>{`${game.playerName}'s game`}</Link>
+  const renderGames = Object.keys(games).map(gameId =>
+    <li>
+      <Link key={games[gameId].id} to={`/games/${games[gameId].id}`}>{`${games[gameId].playerName}'s game`}</Link>
+    </li>
   );
 
   return (
     <div>
-      {renderGames}
+      <ul>
+        {renderGames}
+      </ul>
     </div>
   );
 };
