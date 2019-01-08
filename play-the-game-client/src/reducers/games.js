233e208eb72b3614e84c268b7fbf3e273f4b13cb
sgibson47 <sgibson47@gmail.com>
2019-01-08 14:32:18 -0600

    create games reducer

---
+++ play-the-game-client/src/reducers/games.js
@@ -0,0 +1,14 @@
+const initialState =
+      {
+        []
+      }
+
+export default (state = initialState, action) => {
+  switch(action.type){
+    case 'GET_GAMES_SUCCESS':
+      return action.games
+      
+    default:
+      return state;
+  }
+}
