01fde63fd0cfdad9deff3ef803e16fbd60b2495c
sgibson47 <sgibson47@gmail.com>
2019-01-10 16:49:38 -0600

    add handleSubmit function with goals in comments; add same to form's onSubmit attribute

--- play-the-game-client/src/components/NewGame.js
+++ play-the-game-client/src/components/NewGame.js
@@ -8,19 +8,28 @@ class NewGame extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event){
     this.setState({playerName: event.target.value})
   }
 
+  handleSubmit(event){
+    event.preventDefault();
+    // some code submitting the form to games#create
+    // dispatching setGame with returned new game info
+    // redirecting to new game's show page 
+  }
+
   render(){
     return(
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label> Player Name:
           <input 
             type='text' 
             value={this.state.playerName}
+            onChange={this.handleChange}
           />
         </label>
         <input type='submit' value='Start New Game'/>
