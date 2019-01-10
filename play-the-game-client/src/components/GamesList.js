2718e01e92d8536cbf736978fc01f9d8fa7dab33
sgibson47 <sgibson47@gmail.com>
2019-01-10 12:07:34 -0600

    refactor GamesList to list out links to the show pages of each of the games passed to it via props

--- play-the-game-client/src/components/GamesList.js
+++ play-the-game-client/src/components/GamesList.js
@@ -2,19 +2,17 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
 class GamesList extends Component{
-  renderGames = (games) =>{
-   games.map(game =>
-       <li><Link key={game.id} to={`/games/${game.id}`}>{`${game.playerName}'s game`}</Link></li>
-    );
-  }
-
+  
   render(){
-    debugger
     return (
       <div>
         <h1>All the Games!</h1>
         <ul>
-         {this.renderGames(this.props.games)}
+          {this.props.games.map(game =>
+            <li><Link key={game.id} to={`/games/${game.id}`}>
+            {game.playerName}'s game
+            </Link></li>
+          )}
         </ul>
       </div>
     );
