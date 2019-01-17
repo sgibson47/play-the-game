299be5b0ed5a1eb44dd07ee57b5658d966ada448
sgibson47 <sgibson47@gmail.com>
2019-01-17 13:16:50 -0600

    remove unused code

--- play-the-game-api/app/models/game.rb
+++ play-the-game-api/app/models/game.rb
@@ -5,9 +5,4 @@ class Game < ApplicationRecord
   has_many :cards, :through => :piles
   has_many :cards, :through => :deck
   has_many :cards, :through => :hand
-
-  accepts_nested_attributes_for :piles
-  accepts_nested_attributes_for :deck
-  accepts_nested_attributes_for :hand
-  accepts_nested_attributes_for :cards
 end
