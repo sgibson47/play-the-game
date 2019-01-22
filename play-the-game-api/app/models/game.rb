e37a77c09c1f6aca6c31d40be2596b4f0e5f2172
sgibson47 <sgibson47@gmail.com>
2019-01-22 12:30:30 -0600

    remove game model's unused has_many through associations

--- play-the-game-api/app/models/game.rb
+++ play-the-game-api/app/models/game.rb
@@ -2,9 +2,6 @@ class Game < ApplicationRecord
   has_many :piles
   has_one :deck
   has_one :hand
-  has_many :cards, :through => :piles
-  has_many :cards, :through => :deck
-  has_many :cards, :through => :hand
 
   def playedAtLeastTwo
     if self.moves <2
