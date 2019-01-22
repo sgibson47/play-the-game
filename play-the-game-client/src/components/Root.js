0266483009e7d84d4c555ccd6a096a88970b4bdb
sgibson47 <sgibson47@gmail.com>
2019-01-22 13:40:43 -0600

    remove comments

--- play-the-game-client/src/components/Root.js
+++ play-the-game-client/src/components/Root.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react'
 
 class Root extends Component {
   render(){
-    // console.log("From Root")
-    // debugger
     return (
       <div className='Root'>
         <h2>Welcome to Play The Game</h2>
