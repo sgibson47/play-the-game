bfedd4f0555757ed9ddd0e40e30e6777e007a667
sgibson47 <sgibson47@gmail.com>
2019-01-22 12:40:26 -0600

    remove comments

--- play-the-game-api/app/serializers/pile_serializer.rb
+++ play-the-game-api/app/serializers/pile_serializer.rb
@@ -1,8 +1,6 @@
 class PileSerializer < ActiveModel::Serializer
   attributes :id, :asc, :game_id, :topMostCard
 
-  # has_many :cards, polymorphic: true
-
   def topMostCard
     object.cards.mostRecentlyUpdated.one[0]
   end
