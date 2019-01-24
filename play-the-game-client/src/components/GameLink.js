05f14c91a7216d1614980c6eedacfff38d24f306
sgibson47 <sgibson47@gmail.com>
2019-01-24 16:01:57 -0600

    Remove button, counter, constructor & handler

--- play-the-game-client/src/components/GameLink.js
+++ play-the-game-client/src/components/GameLink.js
@@ -2,15 +2,6 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
 class GameLink extends Component {
-  constructor(props){
-    super(props);
-    this.state = {count: 0}
-  }
-
-
-  clickHandler = () =>{
-    this.setState({count: this.state.count+1})
-  }
 
   render(){
     return (
@@ -18,8 +9,6 @@ class GameLink extends Component {
         <Link to={`/games/${this.props.game.id}`}>
           {`${this.props.game.playerName}'s game`}
         </Link>
-        <button onClick={this.clickHandler}>Vote</button>
-        <div>{this.state.count}</div>
       </div>
     );
   }
