b1caa0ddec2f644d7c9915a4a8a52978383f60e0
sgibson47 <sgibson47@gmail.com>
2019-01-31 13:58:22 -0600

    import Deck; render Deck and H1 with playerName

--- play-the-game-client/src/containers/HeaderAndDeck.js
+++ play-the-game-client/src/containers/HeaderAndDeck.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import Deck from '../components/Deck'
 
 class HeaderAndDeck extends Component {  
 
   render(){
     return(
       <div className="HeaderAndDeck">
-        
+        <h1 className='GameH1'>{this.props.player}'s Game</h1>
+        <Deck cardsLeft={this.props.cardsLeft}/>
       </div>
     )
   }
