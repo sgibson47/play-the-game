9e616c963d6e3969614f210348ec91fc9d0f60a7
sgibson47 <sgibson47@gmail.com>
2019-01-31 16:22:44 -0600

    remove some text

--- play-the-game-client/src/containers/Piles.js
+++ play-the-game-client/src/containers/Piles.js
@@ -5,7 +5,7 @@ class Piles extends Component {
   render(){
     return(
       <div className="piles">
-        <h3>Piles you can play on:</h3>
+        <h3>Piles:</h3>
         {this.props.piles.map(pile =>
           <Pile 
             key={pile.id} 
