6d9e4f94447e2f6b7b6ac1493d4cca2cb2c371f1
sgibson47 <sgibson47@gmail.com>
2019-01-31 13:45:37 -0600

    create new component

---
+++ play-the-game-client/src/containers/HeaderAndDeck.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class HeaderAndDeck extends Component {  
+
+  render(){
+    return(
+      <div className="HeaderAndDeck">
+        
+      </div>
+    )
+  }
+}
+
+export default HeaderAndDeck;
