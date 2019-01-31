6f019ec8a8de30853faa765f5ddd06d3b60fa3e2
sgibson47 <sgibson47@gmail.com>
2019-01-31 15:18:26 -0600

    import and render Piles and HeaderAndDeck

--- play-the-game-client/src/containers/DeckAndPiles.js
+++ play-the-game-client/src/containers/DeckAndPiles.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
+import Piles from './Piles';
+import HeaderAndDeck from './HeaderAndDeck';
 
 class DeckAndPiles extends Component {  
 
   render(){
     return(
       <div className="DeckAndPiles">
-        
+        <HeaderAndDeck
+          cardsLeft={this.props.cardsLeft}
+          player={this.props.player}
+        />
+        <Piles 
+          piles={this.props.piles}
+          selectedPile={this.props.selectedPile}
+          selectPile={this.props.selectPile}
+          deselectPile={this.props.deselectPile}
+        />
       </div>
     )
   }
