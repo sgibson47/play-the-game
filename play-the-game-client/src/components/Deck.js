963cc32107c6e9d81c19ab412d0853f50233ecdf
sgibson47 <sgibson47@gmail.com>
2019-01-31 14:34:31 -0600

    remove div around deckCard; style

--- play-the-game-client/src/components/Deck.js
+++ play-the-game-client/src/components/Deck.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Deck = (props) => (
-  <div className="deck">
+  
     <div className="deckCard">
       <br/>
       <br/>
       {props.cardsLeft}
       <p>Cards left <br/> in the Deck</p>
     </div>
-  </div>
+  
 )
 
 export default Deck
