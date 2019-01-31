f065282b8b4d2871ef5992c207e33ccee30220c6
sgibson47 <sgibson47@gmail.com>
2019-01-31 15:06:25 -0600

    add component

---
+++ play-the-game-client/src/containers/DeckAndPiles.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class DeckAndPiles extends Component {  
+
+  render(){
+    return(
+      <div className="DeckAndPiles">
+        
+      </div>
+    )
+  }
+}
+
+export default DeckAndPiles;
