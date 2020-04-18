c10e42856edf63c393db2ffa7dbf2f6728a10a52
sgibson47 <sgibson47@gmail.com>
2020-04-18 15:02:01 -0500

    convert GameOver

--- play-the-game-client/src/components/GameOver.js
+++ play-the-game-client/src/components/GameOver.js
@@ -1,22 +1,11 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class GameOver extends Component {
-
-  GameOverClassName = () => {
-    if(!this.props.render){
-      return "GameOver"
-    }else {
-      return "HideGameOver"
-    }
-  }
-
-  render(){
-    return (
-      <div className={this.GameOverClassName()} >
-        <h1>GAME OVER</h1>
-      </div>
-    )
-  }
+const GameOver = ({render}) =>{
+  return (
+    <div className={render ? "GameOver" : "HideGameOver"} >
+      <h1>GAME OVER</h1>
+    </div>
+  )
 }
 
 export default GameOver
